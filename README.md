b7916bbb4330595090406a0f055de9351a68f955
Anne van Ede <a.e.vanede@students.uu.nl>
2020-01-17 13:32:21 +0100

    Basic Diversity Extractor

--- README.md
+++ README.md
@@ -11,12 +11,13 @@ usage																		:   [-function_option] [-p parameter parameter]
 -h																			:   Help
             
 Function options:
--extract_jsym [-p feature_definitions.xml extracted_feature_values.xml]		:   Extract features from JSymbolic
--print_all_xml [-p feature_info_values]										:   Print Feature info and values to XML
--print_inf_xml [-p feature_info]											:   Print Feature info to XML
--print_val_xml [-p feature_values]											:   Print Feature values to XML
+-extract_jsym 	[-p feature_definitions.xml extracted_feature_values.xml]	:   Extract features from JSymbolic
+-extract_div  	[-p feature_diversity]                            			:   Extract and Print Feature Diversity
+-print_all_xml 	[-p feature_info_values]									:   Print Feature info and values to XML
+-print_inf_xml 	[-p feature_info]											:   Print Feature info to XML
+-print_val_xml 	[-p feature_values]											:   Print Feature values to XML
 -print_all_csv	[-p feature_info_values]									:   Print Feature info and values to CSV
--change_basedir [-p C:/]												:   View and change base directory
+-change_basedir [-p C:/]													:   View and change base directory
 
 Leave parameter part ([-p ...]) away for default values as show above
 NB: no spaces in filenames allowed.
--- XML Processor/Program.cs
+++ XML Processor/Program.cs
@@ -20,20 +20,24 @@ namespace XML_Processor
             }
         }
 
-        static List<string> newLines = new List<string>();
-        static int listIndex = 0;
-        static int fileIndex = 1;
-        static string[] lines;
+        // Change before distribution
         static string baseString = @"D:\Anne\Documents\Uni\Sound and Music Technology\Data\jSymbolic Data Output\";
 
-        static List<List<string>>[] longFeatures;
-        static int[] featureLength;
+        static string[] functionOptions = new string[] { "-extract_jsym", "-print_all_txt", "-print_inf_txt", "-print_val_txt", "-change_basedir", "-print_all_csv", "-extract_div" };
 
+        static List<string> newLines = new List<string>();
+        static string[] lines;
+
+        static List<List<string>>[] featureValues;
+        static int[] featureLength;
         static List<string> songNames = new List<string>();
         static List<Feature> featInfo = new List<Feature>();
+        static int noSongs, noFeatures;
 
-        static string[] functionOptions = new string[] { "-extract_jsym", "-print_all_xml", "-print_inf_xml", "-print_val_xml", "-change_basedir", "-print_all_csv" };
+        static IDictionary<string, int>[] featureDicts;
 
+        static int listIndex = 0;
+        static int fileIndex = 1;
 
         static void Main(string[] args)
         {
@@ -43,9 +47,9 @@ namespace XML_Processor
             {
                 //Console.Write("<XMLProcessor>");
                 Console.Write(baseString + ">");
-                string opSuccess = "";
-                string[] inputParameters = Console.ReadLine().Split("-p");
-                string param = "", paramOne = "", paramTwo = "";
+                string opResult = "";
+                string[] inputParameters = Console.ReadLine().Split("-p ");
+                string param = "";
                 if (inputParameters.Length > 1)
                     param = inputParameters[1].Trim();
 
@@ -55,44 +59,36 @@ namespace XML_Processor
                         PrintHelp();
                         break;
                     case "-extract_jsym":
-                        if(param != "")
-                        {
-                            string[] parameters = param.Split(' ');
-                            paramOne = parameters[0];
-                            paramTwo = parameters[1];
-                        }
-                        opSuccess += RetrieveFeaturesXML(@"" + paramOne, @"feature_definitions.xml");
-                        opSuccess += RetrieveValuesXML(@"" + paramTwo, @"extracted_feature_values.xml");
+                        opResult += ExtractJSymbolic(param);
                         break;
-                    case "-print_all_xml":
-                        opSuccess += ParseAllToXML(param, "feature_info_values");
+                    case "-extract_div":
+                        opResult += ExtractDiversity(param, "feature_diversity");
                         break;
-                    case "-print_inf_xml":
-                        opSuccess += ParseInfoToXML(param, "feature_info");
+                    case "-print_all_txt":
+                        opResult += ParseAllToTXT(param, "feature_info_values");
                         break;
-                    case "-print_val_xml":
-                        opSuccess += ParseValuesToXML(param, "feature_values");
+                    case "-print_inf_txt":
+                        opResult += ParseInfoToTXT(param, "feature_info");
+                        break;
+                    case "-print_val_txt":
+                        opResult += ParseValuesToTXT(param, "feature_values");
                         break;
                     case "-print_all_csv":
-                        opSuccess += ParseAllToCSV(param, "feature_info_values");
+                        opResult += ParseAllToCSV(param, "feature_info_values");
                         break;
                     case "-change_basedir":
                         baseString = @"" + param;
-                        opSuccess += "Directory changed";
+                        opResult += "Successful - Directory changed";
                         break;
                     default:
                         Console.WriteLine("Function Option not recognized. Input -h for help.");
                         break;
                 }
 
-                if (opSuccess != "") Console.WriteLine("====== Finished - " + opSuccess + " ======");
+                if (opResult != "") Console.WriteLine("====== " + opResult + " ======");
 
             }
 
-
-
-
-            //WriteToFile("extracted_feature_values_cleaned", "xml");
         }
 
         static void Init()
@@ -117,9 +113,10 @@ namespace XML_Processor
 
             Console.WriteLine("Function options:");
             Console.WriteLine(functionOptions[0] + "\t[-p feature_definitions.xml extracted_feature_values.xml]" + " :   Extract features");
-            Console.WriteLine(functionOptions[1] + "\t[-p feature_info_values]" + " \t\t\t\t  :   Print Feature info and values to XML");
-            Console.WriteLine(functionOptions[2] + "\t[-p feature_info]" + " \t\t\t\t\t  :   Print Feature info to XML");
-            Console.WriteLine(functionOptions[3] + "\t[-p feature_values]" + " \t\t\t\t\t  :   Print Feature values to XML");
+            Console.WriteLine(functionOptions[6] + "\t[-p feature_diversity]" + " \t\t\t\t\t  :   Extract and Print Feature Diversity");
+            Console.WriteLine(functionOptions[1] + "\t[-p feature_info_values]" + " \t\t\t\t  :   Print Feature info and values to TXT");
+            Console.WriteLine(functionOptions[2] + "\t[-p feature_info]" + " \t\t\t\t\t  :   Print Feature info to TXT");
+            Console.WriteLine(functionOptions[3] + "\t[-p feature_values]" + " \t\t\t\t\t  :   Print Feature values to TXT");
             Console.WriteLine(functionOptions[5] + "\t[-p feature_info_values]" + " \t\t\t\t  :   Print Feature info and values to CSV");
             Console.WriteLine(functionOptions[4] + "\t[-p C:/]" + " \t\t\t\t\t\t  :   View and change base directory");
             Console.WriteLine("");
@@ -130,7 +127,27 @@ namespace XML_Processor
             Console.WriteLine("");
         }
 
-        static string RetrieveFeaturesXML(string paramName, string defaultName)
+        static string ExtractJSymbolic(string parameter)
+        {
+            string paramOne = "", paramTwo = "";
+            if (parameter != "")
+            {
+                string[] parameters = parameter.Split(' ');
+                if (parameters.Length < 2)
+                {
+                    return "Failed - Parameters invalid";
+                }
+                paramOne = parameters[0];
+                paramTwo = parameters[1];
+            }
+            string resultOne = RetrieveFeaturesJSymbolic(@"" + paramOne, @"feature_definitions.xml");
+            string resultTwo = RetrieveValuesJSymbolic(@"" + paramTwo, @"extracted_feature_values.xml");
+
+
+            return resultOne + " ======\n====== " + resultTwo;
+        }
+
+        static string RetrieveFeaturesJSymbolic(string paramName, string defaultName)
         {
             string fileName = (paramName == "" ? defaultName : paramName);
 
@@ -166,20 +183,20 @@ namespace XML_Processor
                     continue;
                 }
             }
+            noFeatures = featInfo.Count;
+            featureValues = new List<List<string>>[noFeatures];
+            featureLength = new int[noFeatures];
 
-            longFeatures = new List<List<string>>[featInfo.Count];
-            featureLength = new int[longFeatures.Length];
-
-            for (int x = 0; x < longFeatures.Length; x++)
+            for (int x = 0; x < noFeatures; x++)
             {
-                longFeatures[x] = new List<List<string>>();
+                featureValues[x] = new List<List<string>>();
                 featureLength[x] = -1;
             }
 
-            return "Feature Information Retrieved - ";
+            return "Successful - Feature Information Retrieved";
         }
 
-        static string RetrieveValuesXML(string paramName, string defaultName)
+        static string RetrieveValuesJSymbolic(string paramName, string defaultName)
         {
             string fileName = (paramName == "" ? defaultName : paramName);
 
@@ -201,7 +218,7 @@ namespace XML_Processor
                     // name of next feature
                     featureIndex++;
                     featureLength[featureIndex]++;
-                    longFeatures[featureIndex].Add(new List<string>());
+                    featureValues[featureIndex].Add(new List<string>());
 
                     lines[i] = lines[i].Split('>')[1].Split('<')[0];
                 }
@@ -209,7 +226,7 @@ namespace XML_Processor
                 {
                     // value of feature
                     lines[i] = lines[i].Split('>')[1].Split('<')[0];
-                    longFeatures[featureIndex][featureLength[featureIndex]].Add(lines[i]);
+                    featureValues[featureIndex][featureLength[featureIndex]].Add(lines[i]);
                 }
                 else
                 {
@@ -217,71 +234,71 @@ namespace XML_Processor
                 }
             }
 
-
-            return "Feature Values Retrieved";
+            noSongs = songNames.Count;
+            return "Successful - Feature Values Retrieved";
         }
 
-        static string ParseAllToXML(string paramName, string defaultName)
+        static string ParseAllToTXT(string paramName, string defaultName)
         {
             string fileName = (paramName == "" ? defaultName : paramName);
 
             // Parse To Printable
-            for (int x = 0; x < longFeatures.Length; x++)
+            for (int x = 0; x < noFeatures; x++)
             {
                 newLines.Add(featInfo[x].name);
                 newLines.Add(featInfo[x].description);
                 newLines.Add(featInfo[x].dimensions.ToString());
 
-                for (int y = 0; y < longFeatures[x].Count; y++)
+                for (int y = 0; y < featureValues[x].Count; y++)
                 {
                     newLines.Add(songNames[y]);
-                    for (int z = 0; z < longFeatures[x][y].Count; z++)
+                    for (int z = 0; z < featureValues[x][y].Count; z++)
                     {
-                        newLines.Add(longFeatures[x][y][z]);
+                        newLines.Add(featureValues[x][y][z]);
                     }
                 }
             }
 
-            WriteToFile(fileName, "xml");
-            return "Results written to '" + fileName + ".xml'";
+            WriteToFile(fileName, "txt");
+            return "Successful - Results written to '" + fileName + ".txt'";
         }
 
-        static string ParseInfoToXML(string paramName, string defaultName)
+        static string ParseInfoToTXT(string paramName, string defaultName)
         {
             string fileName = (paramName == "" ? defaultName : paramName);
 
             // Parse To Printable
-            for (int x = 0; x < longFeatures.Length; x++)
+            for (int x = 0; x < noFeatures; x++)
             {
                 newLines.Add(featInfo[x].name);
                 newLines.Add(featInfo[x].description);
                 newLines.Add(featInfo[x].dimensions.ToString());
             }
 
-            WriteToFile(fileName, "xml");
-            return "Results written to '" + fileName + ".xml'";
+            WriteToFile(fileName, "txt");
+            return "Successful - Results written to '" + fileName + ".txt'";
         }
 
-        static string ParseValuesToXML(string paramName, string defaultName)
+        static string ParseValuesToTXT(string paramName, string defaultName)
         {
             string fileName = (paramName == "" ? defaultName : paramName);
 
             // Parse To Printable
-            for (int x = 0; x < longFeatures.Length; x++)
+            for (int x = 0; x < noFeatures; x++)
             {
 
-                for (int y = 0; y < longFeatures[x].Count; y++)
+                for (int y = 0; y < featureValues[x].Count; y++)
                 {
                     newLines.Add(songNames[y]);
-                    for (int z = 0; z < longFeatures[x][y].Count; z++)
+                    for (int z = 0; z < featureValues[x][y].Count; z++)
                     {
-                        newLines.Add(longFeatures[x][y][z]);
+                        newLines.Add(featureValues[x][y][z]);
                     }
                 }
             }
 
-            WriteToFile(fileName, "xml");
-            return "Results written to '" + fileName + ".xml'";
+            WriteToFile(fileName, "txt");
+            return "Successful - Results written to '" + fileName + ".txt'";
         }
 
         static string ParseAllToCSV(string paramName, string defaultName)
@@ -289,33 +306,31 @@ namespace XML_Processor
             string fileName = (paramName == "" ? defaultName : paramName);
 
             int songID = 1;
-            string nextLine = "Song ID,Song Name";
+            string nextLine = "\"Song ID\",\"Song Name\"";
 
             //Header of Table
             for (int i = 0; i < featInfo.Count; i++)
             {
-                nextLine += "," + featInfo[i].name;
+                nextLine += ",\"" + featInfo[i].name + "\"";
             }
 
             newLines.Add(nextLine);
-
-            int noSongs = songNames.Count;
-
+            
             for (int y = 0; y < noSongs; y++)
             {
-                nextLine = songID++.ToString() + "," + songNames[y];
-                for (int x = 0; x < longFeatures.Length; x++)
+                nextLine = "\"" + songID++.ToString() + "\",\"" + songNames[y] + "\"";
+                for (int x = 0; x < noFeatures; x++)
                 {
-                    if (longFeatures[x][y].Count == 1)
+                    if (featureValues[x][y].Count == 1)
                     {
-                        nextLine += "," + longFeatures[x][y][0];
+                        nextLine += ",\"\"\"" + featureValues[x][y][0] + "\"\"\"";
                     }
                     else
                     {
-                        nextLine += ",\"" + longFeatures[x][y][0];
-                        for (int z = 1; z < longFeatures[x][y].Count; z++)
+                        nextLine += ",\"" + featureValues[x][y][0];
+                        for (int z = 1; z < featureValues[x][y].Count; z++)
                         {
-                            nextLine += "," + longFeatures[x][y][z];
+                            nextLine += "," + featureValues[x][y][z];
                         }
                         nextLine += "\"";
 
@@ -326,8 +341,72 @@ namespace XML_Processor
                 newLines.Add(nextLine);
             }
 
-            WriteToFile(fileName, "xml");
-            return "Results written to '" + fileName + ".xml'";
+            WriteToFile(fileName, "csv");
+            return "Successful - Results written to '" + fileName + ".csv'";
+        }
+
+        static string ExtractDiversity(string paramName, string defaultName)
+        {
+            string resultOne = RetrieveDiversity();
+            string resultTwo = ParseDiversityToTXT(paramName == "" ? defaultName : paramName);
+
+            return resultOne + " ======\n====== " + resultTwo;
+        }
+
+        static string RetrieveDiversity()
+        {
+            featureDicts = new Dictionary<string, int>[noFeatures];
+
+            for (int x = 0; x < noFeatures; x++)
+            {
+                featureDicts[x] = new Dictionary<string, int>();
+                for (int y = 0; y < featureValues[x].Count; y++)
+                {
+                    // Prepare Feature Value(s)
+                    string featureValue = featureValues[x][y][0];
+                    for (int z = 1; z < featureValues[x][y].Count; z++)
+                    {
+                        featureValue += ", " + featureValues[x][y][z];
+                    }
+
+                    AddToDictionnary(featureDicts[x], featureValue);
+
+                }
+            }
+
+            return "Successful - Diversity Retrieved";
+        }
+
+        static string ParseDiversityToTXT(string fileName)
+        {
+
+            // Parse To Printable
+            for (int x = 0; x < noFeatures; x++)
+            {
+                newLines.Add(featInfo[x].name);
+                newLines.Add("No. Different Values: " + featureDicts[x].Count.ToString());
+
+                foreach (KeyValuePair<string, int> item in featureDicts[x])
+                {
+                    newLines.Add(item.Value + "x " + item.Key);
+                }
+            }
+
+            WriteToFile(fileName, "txt");
+            return "Successful - Results written to '" + fileName + ".txt'";
+        }
+
+        static void AddToDictionnary(IDictionary<string, int> dict, string key)
+        {
+            int result;
+            if (dict.TryGetValue(key, out result))
+            {
+                dict[key] = result + 1;
+            }
+            else
+            {
+                dict.Add(key, 1);
+            }
         }
 
         static void WriteToFile(string baseName, string extension, string index = "")
